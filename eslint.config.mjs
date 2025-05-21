60f172181bc36a565289b4aec55695e9ac5efb7f
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-05-21 18:36:03 +0300

    add solution

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'vue/multi-word-component-names': [
+        'error',
+        {
+          ignores: ['Sidebar', 'Loader', 'Login'],
+        },
+      ],
+      'max-len': [
+        'error',
+        {
+          code: 85,
+          ignoreComments: true, // ігнорувати довгі коментарі
+          ignoreStrings: true, // ігнорувати довгі рядки у лапках
+          ignoreUrls: true, // ігнорувати довгі URL
+        },
+      ],
+      semi: ['error', 'always'],
+    },
+  },
+
+  pluginVue.configs['flat/essential'],
+]);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from "vue";
-import App from './App.vue';
-import 'bulma/css/bulma.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles/index.scss'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
